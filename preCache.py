6efa4cdd1b11877f71d45a485036a6aca126a699
sw897 <sunwei.r@gmail.com>
2013-12-19 09:53:42 +0800

    ec2 dev

--- preCache.py
+++ preCache.py
@@ -28,14 +28,14 @@ if __name__ == '__main__':
     # pool = mp.Pool(processes=mp.cpu_count())
 
     debug = True
-    os.environ['NC_PATH'] = '/Users/sw/github/Marine_Forecast_WebGIS/BeihaiModel_out'
+    os.environ['NC_PATH'] = '/data/data/BeihaiModel_out'
     projection = WebMercatorProjection
     model_caches = {
-        'WRF':{'regions':['NCS'], 'scalar':True, 'legend':True, 'imagetile':True, 'jsontile':True, 'isoline':True, 'extentlimit':False},
-        'SWAN':{'regions':['NCS'], 'scalar':True, 'legend':True, 'imagetile':False, 'jsontile':False, 'isoline':True, 'extentlimit':True},
-        'POM':{'regions':['NCS'], 'scalar':True, 'legend':True, 'imagetile':True, 'jsontile':True, 'isoline':False, 'extentlimit':True},
-        'ROMS':{'regions':['NCS'], 'scalar':True, 'legend':True, 'imagetile':True, 'jsontile':True, 'isoline':False, 'extentlimit':True},
-        'FVCOMSTM':{'regions':['BHS'], 'scalar':True, 'legend':True, 'imagetile':True, 'jsontile':True, 'isoline':False, 'extentlimit':True},
+        'WRF':{'regions':['NCS'], 'scalar':True, 'legend':True, 'imagetile':True, 'jsontile':False, 'isoline':True, 'extentlimit':False},
+        'SWAN':{'regions':['NCS'], 'scalar':True, 'legend':True, 'imagetile':False, 'jsontile':False, 'isoline':False, 'extentlimit':False},
+        'POM':{'regions':['NCS'], 'scalar':True, 'legend':True, 'imagetile':True, 'jsontile':False, 'isoline':False, 'extentlimit':False},
+        'ROMS':{'regions':['NCS'], 'scalar':True, 'legend':True, 'imagetile':True, 'jsontile':False, 'isoline':False, 'extentlimit':False},
+        'FVCOMSTM':{'regions':['BHS'], 'scalar':True, 'legend':True, 'imagetile':True, 'jsontile':False, 'isoline':False, 'extentlimit':False},
     }
 
     for model in model_caches:
